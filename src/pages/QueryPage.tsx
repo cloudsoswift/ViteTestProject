347bec33b0dc1c400c557cd0a087e608ba688c19
cloudsoswift <magic9282m@gmail.com>
2023-03-10 18:01:02 +0900

    d

--- src/pages/QueryPage.tsx
+++ src/pages/QueryPage.tsx
@@ -5,47 +5,69 @@ import {
   useQuery,
 } from "@tanstack/react-query";
 import request, { gql } from "graphql-request";
-import * as React from "react";
+import { useEffect, useState } from "react";
 type Props = {};
+type Film = {
+  title: string,
+  director: string,
+  releaseDate: string,
+  speciesConnection: {
+    species: {
+      name: string,
+      classification: string,
+      homeworld: {
+        name: string,
+      }
+    }
+  }
+};
 const URL = "https://swapi-graphql.netlify.app/.netlify/functions/index";
-const QueryList = ():Object => {
+const QueryList = () => {
   const queryClient = useQueryClient();
-  const getStarwarsData = async () => {
-    const q = gql`
-      query Query {
-        allFilms {
-          films {
-            title
-            director
-            releaseDate
-            speciesConnection {
-              species {
-                name
-                classification
-                homeworld {
-                  name
-                }
-              }
+  const [queryString, setQueryString] = useState("");
+  const DEFAULT_QUERY = `query Query {
+    allFilms {
+      films {
+        title
+        director
+        releaseDate
+        speciesConnection {
+          species {
+            name
+            classification
+            homeworld {
+              name
             }
           }
         }
       }
+    }
+  }`
+  const getGraphQLData = async (): Promise<Array<Film>> => {
+    const q = gql`
+      ${queryString}
     `;
-    const data = await request(URL, q);
-    console.log(data);
-    
-    return data;
+    const data: { allFilms: { films: Array<Film> } } = await request(URL, q);
+    console.log(data?.allFilms);
+
+    return data?.allFilms?.films;
   };
-  const query = useQuery<any>({ queryKey: ["starwars"], queryFn: getStarwarsData });
-  React.useEffect(() => {
-    getStarwarsData();
+  const query = useQuery<Array<Film>, Error>(
+    ["starwars"],
+    getGraphQLData
+  );
+  useEffect(()=>{
+    getGraphQLData();
   }, []);
   return (
     <div>
-      <div>{query.isLoading}</div>
-      <div>{query.isFetching}</div>
-      <div>{query.isRefetching}</div>
-      {/* <div>{query.data.allFilms}</div> */}
+      <div>{query.isLoading ? "로딩중" : "로딩중 아님"}</div>
+      <div>{query.isFetching ? "페칭중" : "페칭중 아님"}</div>
+      <div>{query.isRefetching? "리페칭중" : "리페칭중 아님"}</div>
+      <div className="grid grid-cols-12">
+        <textarea className="col-span-6 border" defaultValue={DEFAULT_QUERY}/>
+        <div className="col-span-6 border">{query.data?.map((film, index)=><div>{index + " " + film.title + " - " + film.director}</div>)}</div>
+      </div>
     </div>
   );
 };
